dd308e6b2532fff7dcf707336614464a75760f43
dibya <dibyachakravorty@gmail.com>
2023-02-11 00:19:25 +0100

    Fix: Missing comma

--- inventory_management_rl/setup.py
+++ inventory_management_rl/setup.py
@@ -11,7 +11,7 @@ setup(name="inventory_management_rl",
       python_requires="~=3.9",
       install_requires=["gym==0.21.0",
                         "ray[rllib]==1.11",
-                        "tensorflow; sys_platform != 'darwin' or platform_machine != 'arm64'"
+                        "tensorflow; sys_platform != 'darwin' or platform_machine != 'arm64'",
                         "tensorflow-macos==2.10.0; sys_platform == 'darwin' and platform_machine == 'arm64'"
                         ],
       )
